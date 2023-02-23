6bbcbc0fce9c1827fff39f9e0f2648ec27349f1b
rotembab <56720556+rotembab@users.noreply.github.com>
2023-02-23 16:41:04 +0200

    added menu functionaliy and close button

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,21 @@
-import React from 'react'
-
-
-
+import { useState } from 'react'
+import React  from 'react'
 
 function Navbar() {
+    const [menu,setMenu] = useState(false);
+
   return (
     <div className='bg-secondary p-2 max-sm:p-4 flex flex-row w-full m-auto'>
         {/* Logo */}
         <img className='m-auto p-1 w-12' alt='GamersHub Logo' src={require('../DevLogo.png')}/>
         {/* Burger menu for mobile */}
-        <img className='hidden max-sm:flex m-auto p-1 w-10 h-10' alt='Menu' src={require('../MenuIcon.png')}/>
+        <img onClick={()=>{setMenu(!menu)}} className='hidden max-sm:flex m-auto p-1 w-10 h-10' alt='Menu' src={require('../MenuIcon.png')}/>
         {/* Burger Menu */}
-        <div className='hidden absolute w-3/4 bg-white left-0 top-1 h-5/6'>This is menu</div>
+        <div className={`${menu?"":"hidden"} sm:hidden absolute w-3/4 bg-white left-0 top-1 h-5/6`}>
+          This is menu
+          {/* Close Button */}
+          <img className='w-9 h-9 absolute right-1 top-1' onClick={()=>{setMenu(!menu)}} src={require('../CloseIcon.png')} alt="Close"/>
+        </div>
         {/* Search bar */}
         <input type="text" className='bg-white rounded-full h-8 w-1/2 max-sm:w-34 m-auto pl-2' placeholder='Search'/>
 

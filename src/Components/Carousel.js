2ace866fa55bb89771aa338488a544b23cbddd1b
Rotem Babani <rotembabmen@gmail.com>
2023-03-11 15:30:09 +0200

    Crousel nextImg and prevImg innit

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,6 +1,23 @@
 import React from 'react'
-export default function Carousel() {
-  return (<div> 
+import { useState } from 'react'
+
+
+
+export default function Carousel(props) {
+  //Props will contain an array called image with one cell to hold src string
+  // and the secound cell holds the alt string.
+  const [img,setImg] = useState(0);
+  function NextImg(){
+    img==props.images.length-1?setImg(0):setImg(img+1);
+  }
+
+  function PreviousImg(){
+    img==0?setImg(props.images.length-1):setImg(img-1);
+  }
+
+  return (
+  <div> 
+    <img src={props.images[img][0]} alt={props.images[img][1]}/>
   </div>
   )
 }
